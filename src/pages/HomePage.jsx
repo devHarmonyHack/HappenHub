89ae112214fdcafe203daa29191abc873cd522c4
“elisejonkers” <elisejonkers18@hotmail.com>
2023-11-16 17:48:55 +0100

    render events at HomePage and Link to EventDetailPage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,36 @@
 import axios from "axios";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 function HomePage() {
-  const [eventDetails, setEventDetails] = useState();
+  const [eventDetails, setEventDetails] = useState([]);
 
   const getAllEvents = () => {
-    axios.get(import.meta.env.VITE_API_URL + "events")
-        .then((response) => {
-            console.log(response.data)
-        })
-        .catch((error) => {
-            console.log("error: " + error)
-        });
+    axios
+      .get(import.meta.env.VITE_API_URL + "events")
+      .then((response) => {
+        setEventDetails(response.data);
+      })
+      .catch((error) => {
+        console.log("error: " + error);
+      });
   };
 
-  getAllEvents();
+  useEffect(() => {
+    getAllEvents();
+  }, []);
+
+  return eventDetails.map((event) => {
+    return (
+      <div className="HomePage" key={event.id}>
+        <NavLink to="/event/:eventId">
+          <h2>{event.name}</h2>
+          <p>{event.date}</p>
+          <p>Created by: {event.creator}</p>
+        </NavLink>
+      </div>
+    );
+  });
 }
 
 export default HomePage;
