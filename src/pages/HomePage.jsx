01fdf8b94c395b7f654cda33f42940f882c9d0af
“elisejonkers” <elisejonkers18@hotmail.com>
2023-11-16 17:34:20 +0100

    insert the .env

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,21 @@
-function HomePage () {
+import axios from "axios";
+import { useState } from "react";
 
+
+function HomePage() {
+  const [eventDetails, setEventDetails] = useState();
+
+  const getAllEvents = () => {
+    axios.get(import.meta.env.VITE_API_URL + "events")
+        .then((response) => {
+            console.log(response.data)
+        })
+        .catch((error) => {
+            console.log("error: " + error)
+        });
+  };
+
+  getAllEvents();
 }
 
-export default HomePage
+export default HomePage;
