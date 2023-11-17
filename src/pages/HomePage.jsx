f8405491cf10ae7bf340301545c75b507ad6c3e2
“elisejonkers” <elisejonkers18@hotmail.com>
2023-11-17 12:16:58 +0100

    add functionality to sort by date on HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,35 +3,70 @@ import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function HomePage() {
-  const [eventDetails, setEventDetails] = useState([]);
+  const [events, setEvents] = useState([]);
 
   const getAllEvents = () => {
     axios
       .get(import.meta.env.VITE_API_URL + "events")
       .then((response) => {
-        setEventDetails(response.data);
+        setEvents(response.data);
       })
       .catch((error) => {
         console.log("error: " + error);
       });
   };
 
+  const sortByDate = () => {
+    const toSortByDate = [...events]
+     const sortedByDate = toSortByDate.sort( (a, b) => {
+      return new Date(a.date) - new Date (b.date)
+    })
+    setEvents(sortedByDate)  
+  }
+
   useEffect(() => {
     getAllEvents();
   }, []);
 
-  return eventDetails.map((event) => {
-    return (
-      <div className="HomePage" key={event.id}>
-        <h2>{event.name}</h2>
+  return (
+    <div className="HomePage">
+      <button onClick={ () => { sortByDate () }}>Sort by Date</button>
+
+     {events.map( (event) => {
+      return (
+        <div className="events" key={event.id}>
+          <h2>{event.name}</h2>
           <p>{event.date}</p>
           <p>Created by: {event.creator}</p>
-        <NavLink to= {`/events/${event.id}`}>
-          Check the details of this Event here!
-        </NavLink>
-      </div>
-    );
-  });
+          <NavLink to= {`/events/${event.id}`}>
+           Check the details of this Event here!
+         </NavLink>
+        </div>
+      )
+     })}
+
+    </div>
+  )
+
+
+
+  // return events.map((event) => {
+  //   return (
+  //     <div className="HomePage">
+
+      
+  //     <div className="events" key={event.id}>
+        
+  //       <h2>{event.name}</h2>
+  //         <p>{event.date}</p>
+  //         <p>Created by: {event.creator}</p>
+  //       <NavLink to= {`/events/${event.id}`}>
+  //         Check the details of this Event here!
+  //       </NavLink>
+  //     </div>
+  //     </div>
+  //   );
+  // });
 }
 
 export default HomePage;
