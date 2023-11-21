fa6c896eb58b39cdc5d7e6c01d4261587cf4b86e
“elisejonkers” <elisejonkers18@hotmail.com>
2023-11-21 15:00:21 +0100

    insert functionality of searchbar UserPage

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,22 +1,24 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import "../pages/UserPage.css"
+import "../pages/UserPage.css";
 
 function UserPage() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([])
+  const [query, setQuery] = useState("")
   const colors = ["#F08D7E", "#EFA18A", "#E2BAB1", "#DDA6B9", "#ACAEC5"];
-  const defaultImage = "src/assets/default-profile-picture.png"
+
+  const filteredUsers = users.filter( (user) => {
+    return user.userName.toLowerCase().includes(query.toLowerCase())
+  })
 
   const getAllUsers = () => {
     axios
       .get(import.meta.env.VITE_API_URL + "users")
       .then((response) => {
-        
-        setUsers(response.data)
-        
+        setUsers(response.data)        
       })
       .catch((error) => {
-        console.log("error: " + error)
+        console.log("error: " + error);
       });
   };
 
@@ -26,26 +28,32 @@ function UserPage() {
 
   const getRandomColor = () => {
     return colors[Math.floor(Math.random() * colors.length)];
-  }
+  };
 
   return (
     <div className="UserPage">
-        {users.map( (user) => {
-            const randomColor = getRandomColor()
-            return (
-                <div className="users" key={user.id} style={{backgroundColor: randomColor}}>
-                    <img className="user-img"
-                    src={user.image}
-                    alt="user img"
-                    />
-                    <h2>{user.userName}</h2>
-
-
-                </div>
-            )
-        })}
+      <label htmlFor="">
+        Search:
+        <input 
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+        type="search" />
+      </label>
+      {filteredUsers.map( (user) => {
+        const randomColor = getRandomColor();
+        return (
+          <div
+            className="users"
+            key={user.id}
+            style={{ backgroundColor: randomColor }}
+          >
+            <img className="user-img" src={user.image} alt="user img" />
+            <h2>{user.userName}</h2>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
 export default UserPage;
