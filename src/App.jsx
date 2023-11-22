01326dbf4799e6b9f5e790f8144b238cc7be850b
Fran Martinez <pmg.webdev@gmail.com>
2023-11-22 16:08:06 +0100

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,59 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Header from './components/Header'
-import HomePage from './pages/HomePage'
-import EventDetailsPage from './pages/EventDetailsPage'
-import AddNewEventPage from './pages/AddNewEventPage'
-import Footer from './components/Footer'
-import UserPage from './pages/UserPage'
-import UserDetails from './pages/UserDetailsPage'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import EventDetailsPage from "./pages/EventDetailsPage";
+import AddNewEventPage from "./pages/AddNewEventPage";
+import Footer from "./components/Footer";
+import UserPage from "./pages/UserPage";
+import UserDetails from "./pages/UserDetailsPage";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
   const [events, setEvents] = useState([]);
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
-  function getEvents(){
+  function getEvents() {
     axios
       .get(import.meta.env.VITE_API_URL + "events")
       .then((response) => {
         setEvents(response.data);
-       })
+      })
       .catch((error) => {
         console.log("error: " + error);
-      })};
-   
-      function getUsers() {
-        axios
-          .get(import.meta.env.VITE_API_URL + "users")
-          .then((result) => {
-            setUsers(result.data);
-          })
-          .catch((error) => {
-            console.log("error: " + error);
-          });
-      }
+      });
+  }
 
-   useEffect( () => {
+  function getUsers() {
+    axios
+      .get(import.meta.env.VITE_API_URL + "users")
+      .then((result) => {
+        setUsers(result.data);
+      })
+      .catch((error) => {
+        console.log("error: " + error);
+      });
+  }
+
+  useEffect(() => {
     getEvents();
-    getUsers();        
-  }, [])
-  
+    getUsers();
+  }, []);
+
   return (
     <>
       <Header />
 
       <Routes>
-        <Route path="/" element={<HomePage events={events}/>} />
-        <Route path="/events/:eventId" element={<EventDetailsPage events={events} users={users} />} />
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/events/:eventId"
+          element={<EventDetailsPage  />}
+        />
         <Route path="/add-event" element={<AddNewEventPage />} />
-        <Route path="/users" element={<UserPage allUsers={users}/>} />
-        <Route path="/users/:userId" element={<UserDetails allUsers={users}/>} />
-        
+        <Route path="/users" element={<UserPage />} />
+        <Route path="/users/:userId" element={<UserDetails />} />
       </Routes>
 
       <Footer />
@@ -59,4 +61,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
